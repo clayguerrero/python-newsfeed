49643f78742ebc103ba8993cdfa05429007acbbe
clayg <claytondguerrero@gmail.com>
2024-02-01 15:33:15 -0600

    db is  set up properly

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,12 +1,18 @@
 from datetime import datetime
 from app.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from .Vote import Vote
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
+from sqlalchemy.orm import relationship, column_property
 
 
 class Post(Base):
     user = relationship('User')
     comments = relationship('Comment', cascade='all,delete')
+    votes = relationship('Vote', cascade='all,delete')
+    vote_count = column_property(
+        select(func.count(Vote.id)).where(Vote.post_id == id)
+    )
+
     __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
